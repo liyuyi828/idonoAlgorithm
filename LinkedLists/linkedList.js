944c93853c0bd16d2e139abfd8b3fa42618d838c
Yuyi <liyuyi828@gmail.com>
2017-08-17 15:06:22 -0700

    implement linkedList class

--- LinkedLists/linkedList.js
+++ LinkedLists/linkedList.js
@@ -13,9 +13,27 @@ class LinkedList {
   }
 
   contains(val) { // Returns a boolean indicating whether the input value is present
+    let current = this.head
+    while (current) {
+      if (current.value === val) {
+        return true
+      }
+      current = current.next
+    }
+    return false
   }
 
   indexOf(val) { // Returns the numerical order or "index" of the node with value. Returns -1 if not present
+    let current = this.head
+    let index = 0
+    while (current) {
+      if (current.value === val) {
+        return index
+      }
+      current = current.next
+      index++
+    }
+    return -1
   }
 
   addToHead(val) { // Adds a node to head
@@ -40,5 +58,43 @@ class LinkedList {
 
   insertAfter(node, val) { 
     // Given a node and value, it acts similar to addToTail, except it creates a node after the input node
+    let newNode = new Node(val)
+    let nextNode = node.next
+    node.next = newNode
+    if (node === this.tail) {
+      this.tail = newNode
+    } 
   }
+}
+
+function LinkedListForEach(list, callback) {
+  let current = list.head
+  while (current) {
+    callback(current)
+    current = current.next
+  }  
+}
+
+let list = new LinkedList()
+list.addToTail(1)
+list.addToTail(2)
+list.addToTail(3)
+LinkedListForEach(list, (node) => console.log(node.value)) // 1, 2, 3
+console.log(list.contains(1)) // true
+console.log(list.contains(4)) // false
+console.log(list.indexOf(2)) // 1
+console.log(list.indexOf(4)) // -1
+list.addToHead(100)
+LinkedListForEach(list, (node) => console.log(node.value)) // 100, 1, 2, 3
+console.log(list.contains(100)) // true
+console.log(list.contains(2)) // true
+console.log(list.contains(4)) // false
+console.log(list.indexOf(100)) // 0
+console.log(list.indexOf(2)) // 2
+console.log(list.indexOf(4)) // -1
+
+module.exports = {
+  LinkedList,
+  LinkedListForEach,
+  Node,
 }
