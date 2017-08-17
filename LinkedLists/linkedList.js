00c60c491dda7571c36530ec0777770430754ad9
Yuyi <liyuyi828@gmail.com>
2017-08-16 23:53:40 -0700

    update linkedList class

--- LinkedLists/linkedList.js
+++ LinkedLists/linkedList.js
@@ -1,3 +1,4 @@
+// This is the basic class for all linkedList related problems
 class Node {
   constructor(value){
     this.value = value
@@ -5,13 +6,39 @@ class Node {
   }
 }
 
-class LinkedList () {
-  constructor(){
+class LinkedList {
+  constructor() {
     this.head = null
     this.tail = null
   }
 
-  addToTail(){
-    
+  contains(val) { // Returns a boolean indicating whether the input value is present
+  }
+
+  indexOf(val) { // Returns the numerical order or "index" of the node with value. Returns -1 if not present
+  }
+
+  addToHead(val) { // Adds a node to head
+    let node = new Node(val)
+    node.next = this.head
+    this.head = node
+    if (this.tail === null) {
+      this.tail = node
+    }
+  }
+
+  addToTail(val) { // Adds a node to tail
+    let node = new Node(val)
+    if (this.head === null) {
+      this.head = node
+    }
+    if(this.tail !== null) {
+      this.tail.next = node
+    }
+    this.tail = node
+  }
+
+  insertAfter(node, val) { 
+    // Given a node and value, it acts similar to addToTail, except it creates a node after the input node
   }
 }
